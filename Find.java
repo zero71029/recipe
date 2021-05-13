45ad3daca57ea92ed41675980d6d8a52ae33edd0
zero71029 <82956789+zero71029@users.noreply.github.com>
2021-05-13 09:40:49 +0800

    Add files via upload

---
+++ Find.java
@@ -0,0 +1,61 @@
+package tw.org.iii.recipe;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Find {
+
+	public static void main(String[] args) {
+		System.out.println("hello Find................");
+		String newEn = "雞胸肉";
+		String UUU = "https://icook.tw/search/" + newEn;
+
+		try {
+			URL url = new URL(UUU);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.connect();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			JSONArray root = new JSONArray();
+			JSONObject row = null;
+			while ((line = reader.readLine()) != null) {
+				if (line.contains("class=\"browse-recipe-link\"")) {
+					row = new JSONObject();
+//        	  System.out.println(line.indexOf("href="));
+					System.out.println("網址:  https://icook.tw"
+							+ line.substring(line.indexOf("href=") + 6, line.indexOf(">", 50) - 1));
+					row.put("address", line.substring(line.indexOf("href=") + 6, line.indexOf(">", 50) - 1));
+				}
+
+				if (line.contains("data-srcset=\"\" src=\"")) {
+//        	  System.out.println(line.indexOf("https://imageproxy.icook.network/"));
+					System.out.println("圖片:" + line.substring(line.indexOf("https://imageproxy.icook.network/"),
+							line.indexOf("data-srcset", 50) - 2));
+					row.put("img", line.substring(line.indexOf("https://imageproxy.icook.network/"),
+							line.indexOf("data-srcset", 50) - 2));
+				}
+
+				if (line.contains("<h2 data-title=")) {
+					System.out.println("菜名:" + line.substring(27, line.indexOf(" c", 27)));
+
+					row.put("name", line.substring(27, line.indexOf(" c", 27)));
+					root.put(row);
+				}
+
+			}
+			System.out.println(root);
+			reader.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("找不到資料");
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ FindDetail.java
@@ -0,0 +1,68 @@
+package tw.org.iii.recipe;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class FindDetail {
+
+	public static void main(String[] args) {
+		System.out.println("hello FindDetail................");
+//		JspContext context =  getJspContext();
+//		JspWriter out = context.getOut();
+		String UUU = "https://icook.tw/recipes/372470";
+		ArrayList<String> cn = new ArrayList();
+		try {
+			URL url = new URL(UUU);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.connect();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			JSONArray root = new JSONArray();
+			JSONObject row = null;
+			JSONObject ingredients = new JSONObject();
+			while ((line = reader.readLine()) != null) {
+				if (line.contains("recipe-step-cover ratio-container ratio-container-4-3 glightbox")) {
+					row = new JSONObject();
+//          	   	  System.out.println(line.indexOf("href="));
+					System.out.println(line.substring(line.indexOf("description: ") + 13,
+							line.indexOf("\" data-track-id", 50) - 1));
+					row.put("address", line.substring(line.indexOf("description: ") + 13,
+							line.indexOf("\" data-track-id", 50) - 1));
+					System.out.println(line.substring(line.indexOf("href=") + 6,
+							line.indexOf(">", 50) - 1));
+					row.put("img", line.substring(line.indexOf("href=") + 6,
+							line.indexOf(">", 50) - 1));
+				root.put(row);
+				}
+				
+				
+				
+				
+				if (line.contains("data-track-id=\"recipe-ingredient\"")) {
+//					ingredients = new JSONObject();
+					System.out.println(line.substring(line.indexOf("data-name=") + 11,
+							line.indexOf("\" href=\"", 50) - 1));
+					ingredients.put("ingredients",line.substring(line.indexOf("data-name=") + 11,
+							line.indexOf("\" href=\"", 50) - 1));
+
+				}
+
+			}
+			root.put(ingredients);
+				System.out.println(root);
+				System.out.println(ingredients);
+				reader.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("找不到資料");
+			e.printStackTrace();
+		}
+	}
+}
